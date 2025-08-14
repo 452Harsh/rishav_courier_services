5e007cb1aad0ae0f6b637930b2c7f2ac5be160b8
452Harsh <harshtuli452@gmail.com>
2025-08-14 23:45:05 +0530

    size

--- app/page.tsx
+++ app/page.tsx
@@ -171,7 +171,7 @@ export default function Home() {
   <iframe
     src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d545.7643169147636!2d75.20074671892476!3d31.820902492420405!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzHCsDQ5JzE2LjUiTiA3NcKwMTInMDUuMyJF!5e0!3m2!1sen!2sin!4v1755194778278!5m2!1sen!2sin"
     width="100%" 
-    height="350"
+    height="500"
     style={{ border: 0 }}
     allowFullScreen
     loading="lazy"
